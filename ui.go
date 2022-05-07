3cc47bb2f51dae0a63f10cb74abba271b006bc7e
Aria Taylor <ari@aricodes.net>
2022-05-07 01:58:55 -0400

    Minor UI tweeaks

--- ui.go
+++ ui.go
@@ -20,6 +20,8 @@ func onPortSelect(ctx *Context) {
 
 func loop(ctx *Context) {
 	ports := availableSerialPorts()
+	g.PushWindowPadding(16.0, 16.0)
+	g.PushFramePadding(8.0, 8.0)
 
 	g.SingleWindow().Layout(
 		g.Row(
@@ -28,16 +30,21 @@ func loop(ctx *Context) {
 				g.InputText(&ctx.Config.TwitchChannel).Size(g.Auto),
 			),
 		),
+		g.Dummy(8.0, 8.0),
 		g.Column(
 			g.Label("Serial Port"),
 			g.Combo("", ports[ctx.SelectedPort], ports, &ctx.SelectedPort).OnChange(func() {
 				onPortSelect(ctx)
 			}),
 		),
-		g.Button("Start Comms").OnClick(func() {
+
+		g.Dummy(8.0, 32.0),
+		g.Row(g.Dummy(-100, 0), g.Button("Start Comms").OnClick(func() {
 			go initComms(ctx)
-		}),
+		})),
 	)
+
+	g.PopStyleV(2)
 }
 
 func uiStart(ctx *Context) {
